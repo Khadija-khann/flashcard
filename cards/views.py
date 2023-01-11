0ca743c732a00eb6a37dbed656a8f2e5a7097960
Khadija Khan <khadija.khxn@gmail.com>
2023-01-10 21:27:18 -0500

    first comit

---
+++ cards/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    CreateView,
+)
+
+from .models import Card
+
+class CardListView(ListView):
+    model = Card
+    queryset = Card.objects.all()
+
+class CardCreateView(CreateView):
+    model = Card
+    fields = ["question", "answer", "box"]
+    success_url = reverse_lazy("card-create")
